08da8b6c6839ae4b20ad85b02c640f357e1f4525
melliinda21 <melinda.galaxy2003@gmail.com>
2024-11-14 19:24:21 -0800

    latihan1 pbo2

---
+++ src/latihan1/pbo2/JOPtionPane.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1.pbo2;
+
+import java.awt.event.ActionEvent;
+import javax.swing.JRootPane;
+
+/**
+ *
+ * @author ASUS
+ */
+class JOPtionPane {
+
+    static boolean YES_OPTION;
+
+    static boolean showConfirmDialog(JRootPane rootPane, ActionEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static boolean showConfirmDialog(JRootPane rootPane, String tanya) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
